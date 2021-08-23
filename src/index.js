01cbfe5eab359cea652edc63ce5e96942aa0b487
RuslanUn <rus516815@gmail.com>
2021-08-24 01:20:58 +0600

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    }
+    var min = array[0];
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    }
+    var max = array[0];
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    }
+    var len = array.length;
+    var total = 0;
+    for (var i = 0; i < len; i++) {
+        total += array[i];
+    }
+    return total / len;
+};
